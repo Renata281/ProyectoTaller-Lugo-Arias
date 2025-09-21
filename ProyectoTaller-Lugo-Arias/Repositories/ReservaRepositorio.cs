6bdb80b4e76d1f3c6cbbe474cf6daa3c1a6e8eae
Renata <lugorenata26@gmail.com>
2025-09-20 21:38:02 -0300

    conexion form reserva al mainview

--- ProyectoTaller-Lugo-Arias/Repositories/ReservaRepositorio.cs
+++ ProyectoTaller-Lugo-Arias/Repositories/ReservaRepositorio.cs
@@ -36,12 +36,72 @@ namespace ProyectoTaller_Lugo_Arias.Repositories
 
         public IEnumerable<ReservaModel> GetAll()
         {
-            throw new NotImplementedException();
+            var reservaList = new List<ReservaModel>();
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                //selecciona todos los usuarios ordenados por id_usuario descendente
+                command.CommandText = "SELECT * FROM reserva";
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var reservaModel = new ReservaModel();
+                        reservaModel.Nro_reserva = reader["nro_reserva"] is DBNull ? 0 : (int)reader["nro_reserva"];
+                        reservaModel.Fecha_ingreso = reader["fecha_ingreso"] is DBNull ? DateTime.MinValue : (DateTime)reader["fecha_ingreso"];
+                        reservaModel.Fecha_salida = reader["fecha_salida"] is DBNull ? DateTime.MinValue : (DateTime)reader["fecha_salida"];
+                        reservaModel.Monto_total = reader["monto_total"] is DBNull ? 0 : (decimal)reader["monto_total"];
+                        reservaModel.Id_cliente = reader["id_cliente"] is DBNull ? 0 : (int)reader["id_cliente"];
+                        reservaModel.Dni = reader["dni"] is DBNull ? 0 : (int)reader["dni"];
+                        reservaModel.Nro_habitacion = reader["nro_habitacion"] is DBNull ? 0 : (int)reader["nro_habitacion"];
+                        reservaModel.Id_piso = reader["id_piso"] is DBNull ? 0 : (int)reader["id_piso"];
+                        reservaModel.Estado = reader["estado"] as string ?? string.Empty;
+                        //agregar a la lista
+                        reservaList.Add(reservaModel);
+                    }
+                }
+            }
+            return reservaList;
         }
 
         public IEnumerable<ReservaModel> GetByValue(string valorBusqueda)
         {
-            throw new NotImplementedException();
+            var reservaList = new List<ReservaModel>();
+            int reservaNro = int.TryParse(valorBusqueda, out var id) ? id : 0;
+            string reserva = valorBusqueda;
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = @"SELECT * FROM reserva
+                                      WHERE nro_reserva=@id ";
+
+                command.Parameters.Add("@id", SqlDbType.Int).Value = reservaNro;
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var reservaModel = new ReservaModel();
+                        reservaModel.Nro_reserva = reader["nro_reserva"] is DBNull ? 0 : (int)reader["nro_reserva"];
+                        reservaModel.Fecha_ingreso = reader["fecha_ingreso"] is DBNull ? DateTime.MinValue : (DateTime)reader["fecha_ingreso"];
+                        reservaModel.Fecha_salida = reader["fecha_salida"] is DBNull ? DateTime.MinValue : (DateTime)reader["fecha_salida"];
+                        reservaModel.Monto_total = reader["monto_total"] is DBNull ? 0 : (decimal)reader["monto_total"];
+                        reservaModel.Id_cliente = reader["id_cliente"] is DBNull ? 0 : (int)reader["id_cliente"];
+                        reservaModel.Dni = reader["dni"] is DBNull ? 0 : (int)reader["dni"];
+                        reservaModel.Nro_habitacion = reader["nro_habitacion"] is DBNull ? 0 : (int)reader["nro_habitacion"];
+                        reservaModel.Id_piso = reader["id_piso"] is DBNull ? 0 : (int)reader["id_piso"];
+                        reservaModel.Estado = reader["estado"] as string ?? string.Empty;
+                        //agregar a la lista
+                        reservaList.Add(reservaModel);
+                    }
+                }
+            }
+             return reservaList;
         }
     }
 }
